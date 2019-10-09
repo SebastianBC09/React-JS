d1b1e0de651c94455471644837cad6cb86be884e
Sebastian <sebastian.ballenc@gmail.com>
2019-10-08 22:58:28 -0500

    Updates

---
+++ precache-manifest.029e1458e1482ca996eb16cf1be8e47d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fcb4ebeaa73cba731d9571110c00ad",
+    "url": "/React-JS/index.html"
+  },
+  {
+    "revision": "6de22c4c3218c3e46794",
+    "url": "/React-JS/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "38b72b0f568e87570524",
+    "url": "/React-JS/static/css/main.1f4b55c6.chunk.css"
+  },
+  {
+    "revision": "6de22c4c3218c3e46794",
+    "url": "/React-JS/static/js/2.2b2f760f.chunk.js"
+  },
+  {
+    "revision": "38b72b0f568e87570524",
+    "url": "/React-JS/static/js/main.cdae0603.chunk.js"
+  },
+  {
+    "revision": "f7196ec572426b40586a",
+    "url": "/React-JS/static/js/runtime-main.5aaf9bd2.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/React-JS/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/React-JS/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/React-JS/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/React-JS/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/React-JS/static/media/stars.1d17360a.svg"
+  }
+]);
